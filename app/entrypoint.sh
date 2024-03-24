119eba166f7aa25333693989898f7a9cf3d78b7a
ilfey <ilfey.global@gmail.com>
2024-03-24 22:37:37 +0700

    add postgres start loading

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -2,6 +2,17 @@
 
 cd app
 
+if [ "$SQL_ENGINE" = "django.db.backends.postgresql" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
 if [ "$DEBUG" == "true" ]
 then
     python3 manage.py runserver 0.0.0.0:8000
