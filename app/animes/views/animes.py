29cd8544cd0cfbcfe57905b35e3e9d759037e896
ilfey <ilfey.global@gmail.com>
2024-04-12 23:18:13 +0700

    add route `animes/{id}/rate`

--- app/animes/views/animes.py
+++ app/animes/views/animes.py
@@ -1,10 +1,15 @@
 from django_filters import rest_framework as dj_filters
 
 from rest_framework import viewsets, pagination, filters
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.decorators import action
 
 from animes.serializers.animes import AnimeSerializer, AnimeListSerializer
 from animes import models
 
+from accounts.models import Rate
+from accounts.serializers.rates import RateSerializer
 
 class AnimePagination(pagination.PageNumberPagination):
     page_size = 50
@@ -21,6 +26,7 @@ class AnimeViewSet(viewsets.ReadOnlyModelViewSet):
         filters.SearchFilter,
         filters.OrderingFilter,
     )
+    lookup_url_kwarg = "anime_pk"
     search_fields = (
         "id",
         "title",
@@ -57,3 +63,19 @@ class AnimeViewSet(viewsets.ReadOnlyModelViewSet):
             return AnimeSerializer
 
         return super().get_serializer_class(*args, **kwargs)
+
+    def get_permissions(self):
+        if self.action in ["create", "update", "partial_update", "destroy", "rate_detail"]:
+            return [IsAuthenticated()]
+
+        return super().get_permissions()
+
+    @action(methods=["GET"], detail=True, url_path="rate")
+    def rate_detail(self, request, anime_pk):
+        if not Rate.objects.filter(user=request.user, anime_id=anime_pk).exists():
+            return Response({"detail": "Rate not found"}, status=404)
+
+        data = Rate.objects.filter(user=request.user, anime_id=anime_pk).get()
+        serializer = RateSerializer(data)
+
+        return Response(serializer.data)
